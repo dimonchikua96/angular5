11c7f8147d34a3e6a2ea1c5fae84f0e823bfd2ea
AB070389GDS <dimitrij.gavrilenko@a-bank.com.ua>
2018-02-05 17:26:56 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+
+class Task {
+    constructor(public title: string,
+                public state: boolean = false) {
+    }
+}
+
+const tasks: Task[] =
+    [
+        {
+            title: 'Learn Angular 5',
+            state: true
+        },
+        {
+            title: 'Create news portal',
+            state: false
+        }
+    ];
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+    newTodoTitle: string = '';
+
+    tasks: Task[] = tasks;
+
+    addTask() {
+
+        let todo: Task = new Task(this.newTodoTitle);
+        tasks.push(todo);
+        this.newTodoTitle = '';
+    }
+
+    toggle(task: Task) {
+        task.state = !task.state;
+    }
+
+    delete(taskId: number) {
+        tasks.splice(taskId, 1);
+    }
 }
